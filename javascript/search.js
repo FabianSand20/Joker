2415458413dbb6f63e867205d223343fb4e629a5
FabianSand20 <80415024+FabianSand20@users.noreply.github.com>
2023-05-22 11:31:18 -0600

    Clear code

--- javascript/search.js
+++ javascript/search.js
@@ -3,14 +3,11 @@ import { searchJokesByTerm } from './api.js';
 
 export async function performSearch(searchTerm) {
   const searchButton = document.getElementById('search-button');
-  // const searchInput = document.getElementById('search-input');
   const resultsContainer = document.getElementById('results-container');
-  // const searchTerm = searchInput.value;
 
   const jokeDiv = document.querySelector(".container .joke p");
   const resultsJoke = await searchJokesByTerm(searchTerm);
 
-  // jokeDiv.innerHTML = joke;
 
   if (resultsJoke.length > 0) {
     resultsJoke.forEach(joke => {
@@ -27,41 +24,3 @@ export async function performSearch(searchTerm) {
   }
   console.log(resultsJoke);
 }
-
-
-
-
-// const searchButton = document.getElementById('search-button');
-// const searchInput = document.getElementById('search-input');
-// const resultsContainer = document.getElementById('results-container');
-
-// export function performSearch() {
-//   const resultsJokes = await searchJokesByTerm();
-
-//   console.log("function")
-//   const searchTerm = searchInput.value;
-//   resultsContainer.innerHTML = '';
-
-
-//   await searchJokesByTerm(`https://icanhazdadjoke.com/search?term=${searchTerm}`)
-//     .then(data => {
-//       const jokes = data.results;
-//       if (jokes.length > 0) {
-//         jokes.forEach(joke => {
-//           const jokeElement = document.createElement('p');
-//           jokeElement.textContent = joke.joke;
-//           resultsContainer.appendChild(jokeElement);
-//         });
-//       } else {
-//         const message = document.createElement('p');
-//         message.textContent = 'No se encontraron chistes para el término de búsqueda.';
-//         resultsContainer.appendChild(message);
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//       const message = document.createElement('p');
-//       message.textContent = 'Ocurrió un error al buscar chistes. Por favor, intenta de nuevo más tarde.';
-//       resultsContainer.appendChild(message);
-//     });
-// }
